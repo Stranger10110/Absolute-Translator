7fd8a734849ca594275a2de77f817521b9b67ac8
Nikita <nekit2699@gmail.com>
2019-05-11 18:22:58 +0300

    Rewritten assembly program to calculate a factorial

--- code.asm
+++ code.asm
@@ -1,15 +1,19 @@
 	START 1000
 
 start:
-m:	mov	ax, number
-	dec	number
-	mov	bx, number
-	mul	bx
-	
-	cmp	number, 1
-	jne	m
-	
-	mov	result, ax
+m:  mov ax, number
+    dec number
+    mov bx, number
+    mul bx
+    
+    dec number
+    mul result
+    mov result, ax
+
+    cmp number, 0
+    jne m
+
+    mov result, ax
 
 number:	WORD	10
 result:	RESW	1
